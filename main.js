f70f895b3e9e525995c5bf15cde0c17fce454dfb
Bruno-sj <106452072+Bruno-sj@users.noreply.github.com>
2022-06-23 16:26:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle){
+    element.addEventListener('click', function(){
+      nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, esconder o menu*/
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links){
+  link.addEventListener('click', function(){
+    nav.classList.remove('show')
+  })
+}
+
+/* BOTAO VOLTAR PARA O TOPO */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+
+
+  if(window.scrollY >= 600) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* WHEN SCROLL */
+
+window.addEventListener('scroll', function () {
+  backToTop()
+ 
+})
